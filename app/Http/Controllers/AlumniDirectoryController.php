c75e38eb8af99cafdb2736a158cf8aad0b3a6e55
sindhishahejad <sindhishahejad@gmail.com>
2026-07-25 19:23:51 +0530

    feat: implement advanced directory filters for location and graduation year

--- app/Http/Controllers/AlumniDirectoryController.php
+++ app/Http/Controllers/AlumniDirectoryController.php
@@ -11,16 +11,32 @@ class AlumniDirectoryController extends Controller
     {
         $query = User::with('profile');
 
-        if ($request->has('search') && $request->search != '') {
+        // General search keyword
+        if ($request->filled('search')) {
             $search = $request->search;
             $query->where(function($q) use ($search) {
                 $q->where('name', 'like', "%{$search}%")
                   ->orWhereHas('profile', function($pq) use ($search) {
                       $pq->where('department', 'like', "%{$search}%")
                         ->orWhere('current_company', 'like', "%{$search}%")
+                        ->orWhere('job_title', 'like', "%{$search}%")
                         ->orWhere('location', 'like', "%{$search}%")
                         ->orWhere('graduation_year', 'like', "%{$search}%");
                   });
+            });
+        }
+
+        // Filter by Graduation Year
+        if ($request->filled('graduation_year')) {
+            $query->whereHas('profile', function($pq) use ($request) {
+                $pq->where('graduation_year', $request->graduation_year);
+            });
+        }
+
+        // Filter by Location
+        if ($request->filled('location')) {
+            $query->whereHas('profile', function($pq) use ($request) {
+                $pq->where('location', 'like', "%{$request->location}%");
             });
         }
 
